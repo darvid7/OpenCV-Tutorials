e53bd23c28eef88fc3b43a4d538513109a840727
darvid7 <bvdarvid@gmail.com>
2016-01-22 13:48:51 +1100

    fixed ocv error
    fixed ocv error

--- OpenCV/test.py
+++ OpenCV/test.py
@@ -2,8 +2,7 @@ import cv2
 
 bee = cv2.imread('bee2.png')
 
-if bee.empty():
-    print 'empty'
+
 cv2.namedWindow('text')
 cv2.imshow('text', bee)
 
--- test/python tutorials/tests.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-bee = cv2.imread('bee2.png')
-
-
-cv2.namedWindow('text')
-cv2.imshow('text', bee)
-
-cv2.waitKey(0)
